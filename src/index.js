f09a1508155da74b8a08b3e205ffb4de860e3d6c
QuenMBar <Qbeast360@gmail.com>
2021-03-01 17:07:18 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+document.addEventListener("DOMContentLoaded", contentLoaded);
+
+function contentLoaded() {
+    const imgDiv = document.getElementById("dog-image-container");
+    fetch(imgUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            const dogImageURLArray = json.message;
+            dogImageURLArray.forEach((url) => {
+                let dogImage = document.createElement("img");
+                dogImage.src = url;
+                imgDiv.append(dogImage);
+            });
+        });
+    const breedUrl = "https://dog.ceo/api/breeds/list/all";
+    const dogList = document.getElementById("dog-breeds");
+
+    fetch(breedUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            const breedURLArray = json.message;
+            for (const property in breedURLArray) {
+                const newDogBreed = document.createElement("li");
+                newDogBreed.innerHTML = property;
+                newDogBreed.className = "dogLi";
+                newDogBreed.addEventListener("click", (e) => {
+                    e.target.style = "color:blue";
+                });
+                dogList.append(newDogBreed);
+            }
+        });
+    const selector = document.getElementById("breed-dropdown");
+    selector.addEventListener("change", (e) => {
+        let listItems = document.getElementsByClassName("dogLi");
+        for (let i = 0; i < listItems.length; i++) {
+            if (listItems[i].innerHTML[0] === e.target.value) {
+                listItems[i].style.display = "";
+            } else {
+                listItems[i].style.display = "none";
+            }
+        }
+    });
+}
